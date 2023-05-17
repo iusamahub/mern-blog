6a86925b22f14c519ebb72ab1fa988b05a3b08c6
Usama Bagwan <usamaas100@gmail.com>
2023-05-17 20:26:07 +0530

    add client folder

---
+++ client/src/pages/Home.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='mb-20'>
+      <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Hello, Welcome to my blog</h1>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, quod
+        eius. Eveniet repellendus aliquam sequi. Quam natus quas, ab iure iusto
+        tempore dolores, nemo, veritatis reprehenderit vero quos? Vel, odio
+        provident? Quam, ratione debitis tenetur molestiae voluptatem nisi
+        praesentium cumque? Ipsum necessitatibus nemo delectus nisi nulla illum
+        quo. Omnis, expedita.
+      </p>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        delectus rem dignissimos vitae porro quasi reiciendis ratione ea
+        accusantium ad recusandae aspernatur soluta neque ullam culpa facilis
+        unde, ab quidem dolorum, quaerat mollitia perferendis! Incidunt adipisci
+        laboriosam repellendus ad modi laudantium temporibus at velit obcaecati
+        recusandae! Placeat ex odit debitis.
+      </p>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+        voluptatibus, modi sed tempore itaque accusantium possimus laudantium
+        placeat architecto maxime, vitae iusto nisi hic. Quasi deserunt,
+        voluptatum quos quam vero delectus aut illum illo libero quas ratione
+        fugiat nobis fugit voluptas placeat doloribus nesciunt neque magni, ea
+        adipisci numquam expedita consectetur. Tempore officia magni quaerat
+        quod nesciunt assumenda quo amet doloremque ipsum iure consequatur,
+        eveniet facilis dolorum mollitia necessitatibus nulla modi eligendi.
+        Molestiae provident eveniet blanditiis a, voluptatem sed laborum
+        mollitia praesentium fugit! Blanditiis recusandae natus quia debitis
+        sint cumque ipsum explicabo eum, assumenda magnam sunt fugit deserunt
+        provident minima.
+      </p>
+    </div>
+  );
+}
+
+export default Home
--- server.js
+++ server.js
@@ -1,57 +1,41 @@
 const express = require('express');
 const app = express();
 const {MongoClient} = require('mongodb');
-// const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json({ extended: false }))
+// app.use(express.json({ extended: false }))
 
-const url = 'mongodb://127.0.0.1:27017/mernblog';
+const url = 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(url);
-// console.log(client)
+const database = 'mernblog';
 
-// const url = 'mongodb://localhost:27017'
-// const dbName = 'mernblog'
-// let db
+// app.get('/api/articles/:name', async (req, res) => {
+//     try {
+//         const articleName = req.params.name;
+//         console.log(articleName);
+//         const conn = await client.connect();
+//         // console.log(conn)
+//         client.db('mernblog');
+//         const articleInfo = await db.collection('articles')
+//         console.log(db.collection)
 
-// MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-//   if (err) return console.log(err)
+//         res.status(200).json(articleInfo);
+//         client.close();
+//     } catch (error) {
+//         res.status(500).json({message: "ERROR", error});
+//     }
+// });
 
-//   // Storing a reference to the database so you can use it later
-//   db = client.db(dbName)
-//   console.log(`Connected MongoDB: ${url}`)
-//   console.log(`Database: ${dbName}`)
-// })
-
-app.get('/api/articles/:name', async (req, res) => {
-    try {
-        const articleName = req.params.name;
-        console.log(articleName);
-        const conn = await client.connect();
-        // console.log(conn)
-        client.db('mernblog');
-        const articleInfo = await db.collection('articles')
-        console.log(db.collection)
-
-        res.status(200).json(articleInfo);
-        client.close();
-    } catch (error) {
-        res.status(500).json({message: "ERROR", error});
-    }
-});
-
-
-
-// const url = 'mongodb://localhost:27017/mernblog';
-// mongoose.connect(url, { useNewUrlParser: true })
-
-// const db = mongoose.connection
-// db.once('open', _ => {
-//   console.log('Database connected:', url)
-// })
-
-// db.on('error', err => {
-//   console.error('connection error:', err)
-// })
 
 app.listen(PORT, () => console.log("Server is listening at PORT 8000"))
+
+app.get('/api/articles/:name',  async (req, res) => {
+    let result=  await client.connect();
+    let db =result.db(database);
+    let collection = db.collection('articles');
+    let response = await collection.find({});
+    console.log(response);
+})
+
+// getdata()
